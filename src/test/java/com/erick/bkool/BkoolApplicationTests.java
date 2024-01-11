9565296eaf5b550fe7f331154e237071bfdc3720
Erick de la Cruz <erickdlcruzcas@gmail.com>
2024-01-11 13:59:47 +0100

    summary
    first commit

---
+++ src/test/java/com/erick/bkool/BkoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erick.bkool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BkoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
